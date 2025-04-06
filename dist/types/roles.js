583b6ff7283b257b892094b6f308801e6d26842d
Ramandeep singh <ramandeepsingh@192.168.1.2>
2025-04-06 16:12:27 +0530

    some change in driver controller

--- dist/types/roles.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Roles = void 0;
-var Roles;
-(function (Roles) {
-    Roles["Admin"] = "admin";
-    Roles["Driver"] = "driver";
-    Roles["Customer"] = "customer";
-})(Roles || (exports.Roles = Roles = {}));
