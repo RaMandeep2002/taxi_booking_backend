7fd21afb1377059665d31cfd47f095f8793a300a
Ramandeep singh <ramandeepsingh@192.168.1.3>
2025-05-12 09:58:38 +0530

    Removed Twilio secret from code

--- src/utils/whatsappMessageSender.ts
+++ src/utils/whatsappMessageSender.ts
@@ -1,26 +1,4 @@
-// import twilio from "twilio";
-
 import axios from "axios";
-
-// const accountSid = "ACe16dc8b1e30e3cf3ad9d7f1cd4d713a8";
-// const authToken = "f52be2976e2aa1c8ef031c8779f596f4";
-// const client = twilio(accountSid, authToken);
-
-// export const sendWhatsappMessage = async (to:string, body: string) =>{
-//     try{
-//         const message = await client.messages.create({
-//             from:`whatsapp:+14155238886`,
-//             to:`whatsapp:${to}`,
-//             body:body,  
-//         });
-//         console.log("Message sent --> ", message.sid);
-//         return message.sid;
-//     }
-//     catch(err){
-//         console.log("Failed To send message: ", err);
-//         throw err;
-//     }
-// }3
 
 const PHONE_ID = process.env.PHONE_NUMBER_ID;
 const AUTH_TOKEN = process.env.WHATSAPP_MESSAGE_TOKEN;
@@ -58,8 +36,7 @@ export const sendWhatsappMessage = async (to: string, date: string, time: string
       Authorization: `Bearer ${AUTH_TOKEN}`,
       "Content-Type": "application/json",
     };
-    console.log(`AUTH_TOKEN --> ${AUTH_TOKEN}`);
-    console.log(`PHONE_ID --> ${PHONE_ID}`);
+
     const response = await axios.post(url, data, { headers });
     console.log("✅ WhatsApp message sent:", response.data);
   } catch (error) {
