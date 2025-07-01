bdca9fe926336e26c1d195f4f199df0a3b56ccc3
Ramandeep singh <ramandeepsingh1511@gmail.com>
2025-07-01 12:38:24 +0530

    add some changes in report api

--- src/cron/scheduler.ts
+++ src/cron/scheduler.ts
@@ -12,3 +12,10 @@ cron.schedule("0 0 1 * *", async () => {
     console.log("📅 Running monthly report cron job...");
     await generateAndSendReport();
 });
+
+
+
+cron.schedule("*/5 * * * * *", async () => {
+    console.log("📅 Running monthly report cron job every 5 seconds...");
+    await generateAndSendReport();
+});
