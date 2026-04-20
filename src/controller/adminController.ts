7749450c4baa3414835f9d39a6e17c774aa395db
Ramandeep singh <ramandeepsingh1511@gmail.com>
2026-04-20 10:26:33 +0530

    add default to false

--- src/controller/adminController.ts
+++ src/controller/adminController.ts
@@ -1160,7 +1160,7 @@ export const gettingReport = async (req: Request, res: Response) => {
         (distance === undefined || distance === null || distance === "" || distance === 0) ||
         (distanceValue < 1) ||
         (totalFare === undefined || totalFare === null || totalFare === "" || totalFare === 0)  || 
-        (booking.isPTDW === true)
+        // (booking.isPTDW === true)
       ) {
 
         // Skip this booking -- do not write row
