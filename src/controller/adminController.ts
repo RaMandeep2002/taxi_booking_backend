edbdc9939471ae26ba7730a6a5272fcb88373317
Ramandeep singh <ramandeepsingh1511@gmail.com>
2026-05-02 16:38:46 +0530

    i have add some functionality

--- src/controller/adminController.ts
+++ src/controller/adminController.ts
@@ -1161,7 +1161,7 @@ export const gettingReport = async (req: Request, res: Response) => {
         (distance === undefined || distance === null || distance === "" || distance === 0) ||
         (distanceValue < 1) ||
         (totalFare === undefined || totalFare === null || totalFare === "" || totalFare === 0) ||
-        (booking.isPTDW === true)
+        (booking.isPTDW == true)
       ) {
 
         // Skip this booking -- do not write row
